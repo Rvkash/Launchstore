0081327b8ecdb8cb1983e4a4b5a6a916ded9544d
Rukash <rafael.ieel@hotmail.com>
2020-07-18 19:25:00 -0300

    database

---
+++ database.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "products" (
+  "id" SERIAL PRIMARY KEY,
+  "category_id" int UNIQUE,
+  "user_id" int UNIQUE,
+  "name" text NOT NULL,
+  "description" text NOT NULL,
+  "old_price" text,
+  "price" int NOT NULL,
+  "quantity" int DEFAULT 0,
+  "status" int DEFAULT 1,
+  "created_at" timestamp DEFAULT 'now()',
+  "update_at" timestamp DEFAULT 'now()'
+);
+
+CREATE TABLE "categories" (
+  "id" init PRIMARY KEY,
+  "name" text NOT NULL
+);
+
+CREATE TABLE "files" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "path" text NOT NULL,
+  "product_id" int UNIQUE
+);
+
+ALTER TABLE "categories" ADD FOREIGN KEY ("id") REFERENCES "products" ("id");
+
+ALTER TABLE "products" ADD FOREIGN KEY ("id") REFERENCES "files" ("id");
