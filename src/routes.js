f96136c1d3049f956f4ae0fc630fee3acfc9d674
Rukash <rafael.ieel@hotmail.com>
2020-07-21 13:17:31 -0300

    add: routes controller

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@ routes.get('/', function (req, res) {
 })
 
 routes.get('/products/create', ProductController.create )
-
+routes.post('/products', ProductController.post)
 routes.get('/ads/create', function (req, res) {
   return res.redirect('/products/create')
 })
