81f160517ced8cdfb4a5f3840de7a8cdd28467c3
Rukash <rafael.ieel@hotmail.com>
2020-07-28 12:08:57 -0300

    add: product id

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -15,17 +15,21 @@ module.exports = {
      	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
       RETURNING id
   	`
+    data.price = data.price.replace(/\D/g, '')
 
     const values = [
       data.name,
-      data.category_id,
+      data.user_id || 1,
       1,
       data.description,
-      data.old_price,
+      data.old_price || data.price,
       data.price,
       data.quantity,
-      data.status
+      data.status || 1
     ]
     return db.query(query, values)
+  },
+  find (id) {
+    return db.query('SELECT * FROM products WHERE id = $1', [id])
   }
 }
