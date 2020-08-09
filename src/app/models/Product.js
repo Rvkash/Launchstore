b3521d64fc3a7aa3266e1a01a5419f600da3d174
Rvkash <rafael.ieel@hotmail.com>
2020-08-09 20:44:57 -0300

    update: refact error integer name

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,48 +1,51 @@
 const db = require('../../config/db')
 
 module.exports = {
-  create (data) {
+  create(data) {
     const query = `
     INSERT INTO products (
-			category_id,
-			user_id,
-			name,
-			description,
-			old_price,
-			price,
-			quantity,
-			status
+        category_id,
+        user_id,
+        name,
+        description,
+        old_price,
+        price,
+        quantity,
+        status
      	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
       RETURNING id
   	`
     data.price = data.price.replace(/\D/g, '')
 
     const values = [
-      data.name,
+      data.category_id,
       data.user_id || 1,
-      1,
+      data.name,
+      //1
       data.description,
       data.old_price || data.price,
       data.price,
       data.quantity,
       data.status || 1
     ]
+
     return db.query(query, values)
+
   },
-  find (id) {
-    return db.query('SELECT * FROM products WHERE id = $1', [id])
+  find(id) {
+    return db.query(`SELECT * FROM products WHERE id = $1`, [id])
   },
-  update (data) {
+  update(data) {
     const query = `
       UPDATE products SET 
-      category_id($1),
+      category_id=($1),
       user_id=($2),
       name=($3),
       description=($4),
       old_price=($5),
       price=($6),
       quantity=($7),
-      status=($8),
+      status=($8)
     WHERE id = $9
     `
 
