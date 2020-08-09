da00142a8041aa0efd2905f62cfc122e7377e81b
Rvkash <rafael.ieel@hotmail.com>
2020-08-09 20:47:48 -0300

    add: db

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -1,9 +1,9 @@
 const db = require('../../config/db')
 
-module.exports = {
-  all () {
-    return db.query(`
-			SELECT * FROM categories
-		`)
+  module.exports = {
+    all () {
+      return db.query(`
+        SELECT * FROM categories
+      `)
+    }
   }
-}
