818ca6462cae3bb283009de195425b05410aff2d
Rvkash <rafael.ieel@hotmail.com>
2020-09-24 19:05:03 -0300

    feat: multer config

--- src/app/middlewares/multer.js
+++ src/app/middlewares/multer.js
@@ -1,27 +1,26 @@
 const multer = require('multer')
 
-//FileName unic Storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '.public/images')
+        cb(null, './public/images')
     },
     filename: (req, file, cb) => {
-        cb(null, `${Date.now().toString()}-$(file.originalName)`) 
+        cb(null, `${Date.now().toString()}-${file.originalname}`)
     }
 })
 
 const fileFilter = (req, file, cb) => {
-    const isAccepted = ['image/png', 'image/jpg', 'image/jpg']
-    .find(acceptedFormat => acceptedFormat = file.mimetype)
+    const isAccepted = ['image/png', 'image/jpg', 'image/jpeg']
+    .find(acceptedFormat => acceptedFormat == file.mimetype)
 
     if(isAccepted) {
-        return cb(null, true); 
+        return cb(null, true)
     }
-    
+
     return cb(null, false)
 }
 
-module.exports = (multer) ({
+module.exports = multer({
     storage,
     fileFilter
 })
