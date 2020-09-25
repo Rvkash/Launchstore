29c4d192e3d9e2fdfeb3e654bc2ad06e6af454b0
Rvkash <rafael.ieel@hotmail.com>
2020-09-25 09:21:55 -0300

    feat: hour minutes config and rm UTC timestamp

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -17,14 +17,18 @@ module.exports = {
   date (timestamp) {
     const date = new Date(timestamp)
 
-    const year = date.getUTCFullYear()
-    const month = `0${date.getUTCMonth() + 1}`.slice(-2) // retorna a data ao contrario
-    const day = `0${date.getUTCDate()}`.slice(-2)
+    const year = date.getFullYear()
+    const month = `0${date.getMonth() + 1}`.slice(-2) // retorna a data ao contrario
+    const day = `0${date.getDate()}`.slice(-2)
+    const hour = date.getHours()
+    const minutes = date.getMinutes()
 
     return {
       day,
       month,
       year,
+      hour,
+      minutes,
       iso: `${year}-${month}-${day}`,
       birthDay: `${day}/${month}`,
       format: `${day}/${month}/${year}`
