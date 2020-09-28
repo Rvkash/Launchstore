eaa00f4d4a290d1b7d76e45ed603cce14d526b5f
Rvkash <rafael.ieel@hotmail.com>
2020-09-28 14:46:15 -0300

    fix: delete files

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -7,7 +7,7 @@ module.exports = {
     INSERT INTO files (
         name,
         path,
-        product_id,
+        product_id
      	) VALUES ($1, $2, $3)
       RETURNING id
       `;
@@ -15,7 +15,7 @@ module.exports = {
     const values = [
       filename, 
       path,
-      product_id,
+      product_id
     ]
 
     return db.query(query, values)
@@ -24,16 +24,16 @@ module.exports = {
   async delete(id) {
     try {
         const result = await db.query(`
-            SELECT * FROM files
-            WHERE id = $1`, [id])
+          SELECT * FROM files
+          WHERE id = $1`, [id])
 
         const file = result.rows[0]
 
         fs.unlinkSync(file.path)
 
         return db.query(`
-            DELETE FROM files
-            WHERE id = $1
+          DELETE FROM files
+          WHERE id = $1
         `, [id])
 
         
