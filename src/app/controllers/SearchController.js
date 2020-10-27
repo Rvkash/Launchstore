bd81bbb82bb65485da2e770b73d57d86dc196018
Rvkash <rafael.ieel@hotmail.com>
2020-10-27 15:06:07 -0300

    add search controller

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -1,32 +1,74 @@
 const Product = require('../models/Product')
-const { formatPrice } = require('../../lib/utils')
+const {
+    formatPrice
+} = require('../../lib/utils')
 
 module.exports = {
-   async index(req, res) {
-        let results = await Product.all()
-        const products = results.rows
+    async index(req, res) {
+        try {
+            let results,
+                params = {}
 
-        if (!products) return results.send("Produto não encontrado!")
+            const {
+                filter,
+                category
+            } = req.query
 
-        async function getImage(productId) {
-            results = await Product.files(productId)
-            const files = results.rows.map(file => 
-                 `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+            if (!filter) return res.redirect("/")
 
-            return files[0]
+            params.filter = filter
 
+            if (category) {
+                params.category = category
+            }
+
+            results = await Product.search(params)
+
+            async function getImage(productId) {
+                results = await Product.files(productId)
+                const files = results.rows.map(file =>
+                    `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+
+                return files[0]
+
+            }
+
+            const productsPromise = results.rows.map(async product => {
+                product.img = await getImage(product.id)
+                product.oldPrice = formatPrice(product.old_price)
+                product.price = formatPrice(product.price)
+                return product
+            })
+
+            const products = await Promise.all(productsPromise)
+
+            const search = {
+                term: req.query.filter,
+                total: products.length
+            }
+
+            const categories = products.map(product => ({
+                id: product.category_id,
+                name: product.category_name
+            })).reduce((categoriesFiltered, category) => {
+
+                const found = categoriesFiltered.some(cat => cat.id == category.id)
+
+                if (!found)
+                    categoriesFiltered.push(category)
+
+                return categoriesFiltered
+            }, [])
+
+
+            return res.render("search/index", {
+                products,
+                search,
+                categories
+            })
+
+        } catch (err) {
+            console.log(err)
         }
-
-        const productsPromise = products.map(async product => {
-            product.img = await getImage(product.id)
-            product.oldPrice = formatPrice(product.old_price)
-            product.price = formatPrice(product.price)
-            return product
-            // if Ternario 
-        }).filter((product, index) => index > 2 ? false : true) 
-            
-
-        const lastAdded = await Promise.all(productsPromise)
-        return res.render("search/index", { products: lastAdded })
     }
 }
