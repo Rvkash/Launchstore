f687d3ce8fde7231f42c114fbfdc7da38f22ee45
Rvkash <rafael.ieel@hotmail.com>
2020-10-27 15:06:53 -0300

    add products models

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -77,5 +77,35 @@ module.exports = {
         SELECT * FROM files
         WHERE product_id = $1
     `, [id])
-}
+},
+  search(params) {
+    const { filter, category } = params
+
+    let query = "",
+      filterQuery = `WHERE`
+
+    if (category) {
+      filterQuery = `${filterQuery}
+      products.category_id = ${category}
+      AND `
+    }
+
+    filterQuery =`
+      ${filterQuery} 
+      products.name ilike '%${filter}%'
+      OR products.description ilike '%${filter}%'
+    `
+
+
+    query = `
+      SELECT products.*,
+        categories.name AS category_name
+      FROM products
+      LEFT JOIN categories ON (categories.id = products.category_id)
+      ${filterQuery}
+      
+    `
+
+    return db.query(query)
+  }
 }
