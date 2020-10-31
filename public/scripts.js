4c47477b7becca746b0302382cfa80728a12e940
Rvkash <rafael.ieel@hotmail.com>
2020-10-31 00:54:12 -0300

    add cpf_cnpj mask

--- public/scripts.js
+++ public/scripts.js
@@ -11,6 +11,29 @@ const Mask = {
       style: 'currency',
       currency: 'BRL'
     }).format(value / 100)
+  },
+  cpfCnpj(value) {
+    value = value.replace(/\D/g, "")
+      if (value.length > 14) 
+        value.slice(0, -1)
+
+     if (value.length > 11) { //cnpj
+        value = value.slice(0, -1)
+
+        value = value.replace(/(\d{2})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1/$2")
+        value = value.replace(/(\d{4})(\d)/, "$1-$2")
+      
+      } else { //cpf
+        value = value.replace(/(\d{2})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1-$2")
+    }
+
+    return value
+
   }
 }
 
