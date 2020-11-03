8a5ee8552d28b24bc8bc3d92948cfe80e11dfa76
Rvkash <rafael.ieel@hotmail.com>
2020-11-03 12:25:50 -0300

    feat: validation error

--- public/scripts.js
+++ public/scripts.js
@@ -184,16 +184,33 @@ const Lightbox = {
 
 const Validate = {
   apply(input, func) {
-     let results = Validate[func](input.value)
-     input.value = results.value
+    Validate.clearErrors(input)
 
-     if (results.error)
-      alert("Error!")
+    let results = Validate[func](input.value)
+    input.value = results.value
+
+    if (results.error)
+      Validate.displayError(input, results.error)
   },
-
+  displayError(input, error) {
+    const div = document.createElement('div')
+    div.classList.add('error')
+    div.innerHTML = error
+    input.parentNode.appendChild(div)
+    input.focus()
+  },
+  clearErrors(input) {
+    const errorDiv = input.parentNode.querySelector(".error")
+    if (errorDiv)
+      errorDiv.remove()
+  },
   isEmail(value) {
     let error = null
-    const mailFormat = /^\w+([\.-]?\w+)*@w\w+([\.-]?\w+)*(\.\w{2,3})+/
+
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if (!value.match(mailFormat))
+      error = "Email inválido"
 
     return {
       error,
