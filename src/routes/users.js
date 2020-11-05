8cf5e6e0b34a48274da1ba919bc3cf6a4d2f2bec
Rvkash <rafael.ieel@hotmail.com>
2020-11-05 16:06:28 -0300

    feat: user register

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,6 @@ const routes = express.Router()
 // const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
 
-
 // //Login Logout
 // routes.get('/login', SessionController.loginForm)
 // routes.get('/login', SessionController.login)
@@ -18,7 +17,7 @@ const UserController = require('../app/controllers/UserController')
 // //user register
 
 routes.get('/register', UserController.registerForm)
-// routes.post('/register', UserController.register)
+routes.post('/register', UserController.register)
 
 // routes.get('/', UserController.show)
 // routes.put('/', UserController.update)
