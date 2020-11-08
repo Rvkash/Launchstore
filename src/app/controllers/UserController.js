29bf7680638729813d012172fdae91ddacc2641d
Rvkash <rafael.ieel@hotmail.com>
2020-11-08 16:05:40 -0300

    feat: register

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,27 +4,7 @@ module.exports = {
   registerForm(req, res) {
     return res.render("users/register")
   },
-  async post(req,res) {
-    const keys = Object.keys(req.body)
-
-        for(let key of keys) {
-            if(req.body[key] == '') {
-               return res.send('Please, fill all the fields!')
-            }
-        }
-        let { email, cpf_cnpj, password, passwordRepeat} = req.body
-        
-        cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
-
-        const user = await user.findOne({ 
-          where: {email },
-          or: {cpf_cnpj}
-        })
-        if(user) return res.send('Usuário já existe')
-
-        if(password != passwordRepeat) 
-          return res.send('Password incorreto')
-
-        return res.send('Correto!')
-     }
+  async post(req, res) {
+      return res.send('passed')      
+    }
   }
