288138dccc32995a4d411fccfd8fe136cd8bb52f
Rvkash <rafael.ieel@hotmail.com>
2020-11-12 12:19:43 -0300

    feat: session FormatCpfCnpj, Cep

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -39,5 +39,36 @@ module.exports = {
       style: 'currency',
       currency: 'BRL'
     }).format(price / 100)
+  },
+  formatCpfCnpj(value) {
+    value = value.replace(/\D/g, "")
+      if (value.length > 14) 
+        value.slice(0, -1)
+
+     if (value.length > 11) { //cnpj
+        value = value.slice(0, -1)
+
+        value = value.replace(/(\d{2})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1/$2")
+        value = value.replace(/(\d{4})(\d)/, "$1-$2")
+      
+      } else { //cpf
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1-$2")
+    }
+
+    return value
+
+  },
+  formatCep(value) {
+    value = value.replace(/\D/g, "")
+    if(value.length > 8 ) 
+      value = value.slice(0, -1)
+      
+    value = value.replace(/(\d{5})(\d)/, "$1-$2")
+    return value
   }
 }
