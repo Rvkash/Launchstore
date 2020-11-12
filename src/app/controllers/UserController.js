a151ed159043b64963a19d34fa0954d036baf17b
Rvkash <rafael.ieel@hotmail.com>
2020-11-12 12:17:02 -0300

    feat: session controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,30 @@
 const User = require('../models/User')
+const { formatCep, formatCpfCnpj} = require('../../lib/utils')
+
 
 module.exports = {
-  registerForm(req, res) {
+  async registerForm(req, res) {
     return res.render("users/register")
   },
+  async show (req,res) {
+    const { userId: id } = req.session
+
+    const user = await User.findOne({ where: id})
+
+    if (!user) return res.render("user/register", {
+      error: 'usuário não encontrado'
+    })
+
+    user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+    user.cep = formatCpfCnpj(user.cep)
+
+    return res.render("user/index", { user })
+  },
   async post(req, res) {
-      return res.send('passed')      
-    }
-  }
+    const userId = await User.create(req.body)
+
+    req.session.userId = userId
+
+    return res.redirect("/users")      
+    },
+}
