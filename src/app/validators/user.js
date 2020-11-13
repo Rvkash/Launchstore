82667f381784832cf99b48a0cbf155c30e40baa8
Rvkash <rafael.ieel@hotmail.com>
2020-11-13 13:54:56 -0300

    feat: show, update function

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,31 +1,78 @@
 const User = require('../models/User')
+const { compare } = require('bcrypt')
 
-async function post(req, res, next) {  
-    const keys = Object.keys(req.body)
-        for(let key of keys) {
-            if(req.body[key] == '') {
-               return res.send('Please, fill all the fields!')
-            }
-        }
-        //check if users exists [email,cpf_cnpj]
-        let { email, cpf_cnpj, password, passwordRepeat } = req.body;
+function checkAllFields(body) {
+  const keys = Object.keys(body)
 
-        cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return {
+        user: req.body,
+        error: 'Preencha todos campos'
+      }
+    }
+  }
+}
+async function show(req, res, next) {
+  const { userId: id } = req.session
 
-        const user = await User.findOne({
-          where: { email },
-          or: { cpf_cnpj },
-        })
-        if(user) return res.render('user/register', {
-          error: 'Usuário já cadastrado'
-        })
-         //check if password match
-        if(password != passwordRepeat) 
-          return res.send('Password incorreto')
+  const user = await User.findOne({ where: {id} })
 
-        next()
+  if (!user) return res.render("users/register", {
+    error: 'Usuário não encontrado'
+  })
+
+  req.user = user 
+  next()
+}
+ async function post(req, res, next)  {
+  const fillAllFields = checkAllFields(req.body) 
+  if(fillAllFields) {
+    return res.render("user/register", fillAllFields)
+  }
+
+  let { email, cpf_cnpj, password, passwordRepeat } = req.body 
+
+  cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+
+  const user = await User.findOne({
+    where: { email },
+    or: { cpf_cnpj }
+  })
+  if (user) return res.render('users/register', {
+    user: req.body,
+    error: 'Usuário já cadastrado'
+  })
+  if (password != passwordRepeat) return res.render('users/register', {
+    user: req.body,
+    error: 'A senha e repetição estão incorretas'
+  })
+    next()
+}
+async function update(req, res, next) {
+  const checkAllFields = checkAllFields(req.body) 
+  if(fillAllFields) {
+    return res.render("user/register", fillAllFields)
+  }
+
+  const { id, password} = req.body
+  if(!password) return res.render("user/index", {
+    user: req.body,
+    error:"Coloque sua senha"
+  })
+  const user = await User.findOne({ where: {id}})
+  const passed = await compare(password, user.password)
+  if(!passed) return res.render("user/index", {
+    user: req.body,
+    error: "Senha incorreta"
+  })
+  req.user = user
+
+  next()
 }
 
 module.exports = {
-  post
+  post,
+  show,
+  update
 }
