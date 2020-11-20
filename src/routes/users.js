629d025da04e082b0e47e9f11ce6af53970092f0
Rvkash <rafael.ieel@hotmail.com>
2020-11-20 03:05:12 -0300

    feat: login user

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,12 +3,13 @@ const routes = express.Router()
 
 const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
-const Validator = require('../app/validators/user')
+const UserValidator = require('../app/validators/user')
+const SessionValidator = require('../app/validators/session')
+
 
 // Login Logout
-
 routes.get('/login', SessionController.loginForm)
-// routes.get('/login', SessionController.login)
+routes.post('/login',SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 // Forgot password 
@@ -21,10 +22,10 @@ routes.post('/logout', SessionController.logout)
 // User register
 
 routes.get('/register', UserController.registerForm)
-routes.post('/register', Validator.post, UserController.post)
+routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/', Validator.show, UserController.show)
-routes.put('/', Validator.update, UserController.update)
+routes.get('/', UserValidator.show, UserController.show)
+routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
 module.exports = routes
