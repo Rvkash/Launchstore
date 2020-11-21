8ae82f8d069639cc8d720bfa30179a53ac905d65
Rvkash <rafael.ieel@hotmail.com>
2020-11-21 11:51:29 -0300

    feat: only users middleware

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const routes = express.Router()
 const multer = require('../app/middlewares/multer')
+
 const ProductController = require('../app/controllers/ProductController')
 const SearchController = require('../app/controllers/SearchController')
+
+const { onlyUsers } = require('../app/middlewares/session')
 
 //Search
 routes.get('/search', SearchController.index)
 
 //Products
 routes.get('/:id/edit', ProductController.edit)
-routes.get('/create', ProductController.create)
+routes.get('/create', onlyUsers, ProductController.create)
 routes.get('/:id', ProductController.show)
-routes.get('/:id/edit', ProductController.edit)
+routes.get('/:id/edit', onlyUsers, ProductController.edit)
 
-routes.post('/', multer.array("photos", 6), ProductController.post) //postar
-routes.put('/', multer.array("photos", 6), ProductController.put)   //att
-routes.delete('/', ProductController.delete)  
+routes.post('/', onlyUsers, multer.array("photos", 6), ProductController.post) //postar
+routes.put('/', onlyUsers, multer.array("photos", 6), ProductController.put)   //att
+routes.delete('/', onlyUsers, ProductController.delete)  
 
 module.exports = routes
