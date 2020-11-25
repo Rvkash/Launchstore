e0210f61c5927c044bc3c582890ca26f0775f4c1
Rvkash <rafael.ieel@hotmail.com>
2020-11-24 22:34:56 -0300

    feat: delete product

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -16,7 +16,7 @@ module.exports = {
     async post(req, res) {
         const keys = Object.keys(req.body)
 
-        for(let key of keys) {
+        for(key of keys) {
             if(req.body[key] == '') {
                return res.send('Please, fill all the fields!')
             }
