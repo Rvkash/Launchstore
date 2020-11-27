fa6b98b9ae1e0b3f216c3ba6d29ef72486cac7f3
Rvkash <rafael.ieel@hotmail.com>
2020-11-27 12:40:00 -0300

    feat: password reset

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto')
+
 module.exports = {
   loginForm(req, res) {
     return res.render("session/login")
@@ -15,6 +17,16 @@ module.exports = {
     return res.render("session/forgot-password")
   },
   forgot(req, res) {
-    
+    const user = req.user
+
+    const token = crypto.randomBytes(20).toString("hex")
+
+    let now = new Date()
+    now = now.setHours(now.getHours() + 1)
+
+    await UIEvent.update(user.id, {
+      reset_token: token,
+      reset_token_expires: now
+    })
   }
 }
