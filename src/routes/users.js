0a9ddc0971d9ac216a361c6695ad79abdaebfbff
Rvkash <rafael.ieel@hotmail.com>
2020-12-27 23:21:40 -0300

    feat: sessionvalidator

--- src/routes/users.js
+++ src/routes/users.js
@@ -16,9 +16,9 @@ routes.post('/logout', SessionController.logout)
 // Forgot password 
 
 routes.get('/forgot-password', SessionController.forgotForm)
-// routes.get('/password-reset', SessionController.resetForm)
+routes.get('/password-reset', SessionController.resetForm)
 routes.post('/forgot-password', SessionValidator.forgot, SessionController.forgot)
-// routes.post('/password-reset', SessionController.reset)
+routes.post('/password-reset', SessionValidator.reset, SessionController.reset)
 
 // User register
 
