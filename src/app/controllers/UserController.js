ae750a787c450f88d87204180102d3a128e32f38
Rvkash <rafael.ieel@hotmail.com>
2021-03-05 15:32:07 -0300

    feat: delete session

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -57,15 +57,16 @@ module.exports = {
 },
   async delete(req, res) {
   try {
-    await User.delete(1)
+    await User.delete(req.body.id)
     req.session.destroy()
-    return res.render("Session/login", {
-      success: "Conta deletada"
+    return res.render("session/login", {
+      success: "Conta deletada com sucesso"
     })
 
   }catch(err) {
     console.error(err)
-    return res.render("user/index", {
+    return res.render("users/index", {
+      user: req.body,
       error: "Erro ao tentar deletar sua conta"
     })
   }
